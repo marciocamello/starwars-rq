0e59cf0c370b36ce14d2390e8f06141eb12b3990
Marcio Andre Avelino do Vale <mvale@smartdigit.pt>
2021-06-30 22:29:00 -0300

    first commit

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = ({ setPage }: any) => {
+    return <nav>
+        <button onClick={() => setPage("planets")}>Planets</button>
+        <button onClick={() => setPage("people")}>People</button>
+    </nav>;
+}
+
+export default Navbar;
---
+++ src/components/Planets/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Planets = () => {
+    return <div>
+        <h2>Planets</h2>
+    </div>;
+}
+
+export default Planets;
